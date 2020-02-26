1449e8491f8c2397a36a27041eba6a663d5fe3a3
Nick Baker <ndbaker1@outlook.com>
2020-02-26 14:51:35 -0600

    removed unnecessary js calls from template

--- dtd-web/js/main.js
+++ dtd-web/js/main.js
@@ -1,35 +1,52 @@
+const apiKey = 'EjKBKGiEKnrhbi-wjpdU-5Ch3Xs8QbL3dKnz3efiJKLLND6qSPoTAH469ah0TQ5C67qQKiLZDo7HNZas-JCEbb0Tz70D-t2pA6SdxgcAUwz2JdwMOZm7LGG7e3RQXnYx';
+const querystring = require('querystring');
+
 $(document).ready(function(){
+    
+    // Makes the Nav-bar stick to the top
+    $("#navigation").sticky();
+    
+    
 
+    /*
+        These JQuery functions really slow down the interface,
+        so im disabling them. Some HTML will be affected,
+        but nothing that manually writing HTML/CSS wont fix
 
- $("#owl-example").owlCarousel({
-    // Most important owl features
-    items : 4,
-    pagination : true,
-    paginationSpeed : 1000,
-    navigation : true,
-    navigationText : ["","<i class='fa fa-angle-right'></i>"],
-    slideSpeed : 800,
- });
-
-	$("#navigation").sticky({
-		topSpacing : 75,
-	});
+    $("#owl-example").owlCarousel({
+        // Most important owl features
+        items : 4,
+        pagination : true,
+        paginationSpeed : 1000,
+        navigation : true,
+        navigationText : ["","<i class='fa fa-angle-right'></i>"],
+        slideSpeed : 800,
+    });
 
 	$('#nav').onePageNav({
 		currentClass: 'current',
-		changeHash: false,
+		changeHash: true,
 		scrollSpeed: 15000,
 		scrollThreshold: 0.5,
 		filter: '',
 		easing: 'easeInOutExpo'
-	});
-
+    });
      $('#top-nav').onePageNav({
          currentClass: 'active',
          changeHash: true,
          scrollSpeed: 1200
     });
-//Initiat WOW JS
+    */
+
+//Initiate WOW JS
     new WOW().init();
 
 });
+
+function search_btn_press() {
+    var description = document.getElementById("description").value;
+    var location = document.getElementById("location").value;
+    console.log(description, location);
+
+
+}
--- dtd-web/yelp.js
+++ dtd-web/yelp.js
@@ -1,5 +1,5 @@
 
-function test() {
+function getYelpData() {
     const yelp = require('yelp-fusion');
 
     const apiKey = 'EjKBKGiEKnrhbi-wjpdU-5Ch3Xs8QbL3dKnz3efiJKLLND6qSPoTAH469ah0TQ5C67qQKiLZDo7HNZas-JCEbb0Tz70D-t2pA6SdxgcAUwz2JdwMOZm7LGG7e3RQXnYx';
@@ -17,7 +17,29 @@ function test() {
         console.log(prettyJson);
     }).catch(e => {
         console.log(e);
-    });    
+    });     
 };
 
 test();
+
+
+yelpRequest() { 
+    var searchRequest = {
+      term: 'Chick-fil-a',
+    }
+    var yelp_corsanywhere = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/";
+    var searchType = 'businesses/search?'
+    var queryParamString = querystring.stringify(searchRequest);
+    const header = {
+      method: 'GET',
+      headers: new Headers({
+        "Authorization": "Bearer "+apiKey
+      })
+    }
+    fetch(yelp_corsanywhere+searchType+queryParamString, header
+    ).then(response => {
+      return response.json();
+    }).then(json => {
+      console.log(json);
+    });
+  }
