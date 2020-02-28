cb8cf364a3436ab807400772c7fc2fdb3f78b88a
Nick Baker <ndbaker1@outlook.com>
2020-02-27 18:05:28 -0600

    search bar fix and visual update

--- dtd-web/js/main.js
+++ dtd-web/js/main.js
@@ -2,10 +2,7 @@
 $(document).ready(function(){
     
     // Makes the Nav-bar stick to the top
-    $("#navigation").sticky();
-    
-    
-
+    $("#navigation").sticky();    
     /*
         These JQuery functions really slow down the interface,
         so im disabling them. Some HTML will be affected,
@@ -42,9 +39,36 @@ $(document).ready(function(){
 
 });
 
+const apiKey = 'EjKBKGiEKnrhbi-wjpdU-5Ch3Xs8QbL3dKnz3efiJKLLND6qSPoTAH469ah0TQ5C67qQKiLZDo7HNZas-JCEbb0Tz70D-t2pA6SdxgcAUwz2JdwMOZm7LGG7e3RQXnYx';
+
+// Yelp api Function calls
+
 function search_btn_press() {
     var description = document.getElementById("description").value;
     var location = document.getElementById("location").value;
-    console.log(description, location);
+    console.log('Search Bar Params: ', description, location);
+    yelpRequest(location, description);
+}
 
+function yelpRequest(loc, desc) { 
+    var yelp_corsanywhere = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/";
+    var searchType = 'businesses/search?';
+    var params = {}
+    params['location']  = loc != '' ? loc : undefined;
+    params['term']      = desc != '' ? desc : undefined;
+
+    const header = {
+        method: 'GET',
+        headers: new Headers({
+            "Authorization": "Bearer "+apiKey
+        })
+    }
+    fetch(
+        yelp_corsanywhere + searchType + $.param(params),
+        header
+    ).then(response => {
+      return response.json();
+    }).then(json => {
+      console.log(json);
+    });
 }
